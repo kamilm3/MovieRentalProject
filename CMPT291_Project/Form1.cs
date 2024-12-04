9c4d4f5171e839a11e23df424267c395dde1ebb7
angelicab20 <anbilliones@gmail.com>
2024-12-03 21:12:32 -0700

    Commented myConnection

--- CMPT291_Project/Form1.cs
+++ CMPT291_Project/Form1.cs
@@ -22,7 +22,7 @@ namespace CMPT291_Project
         {
             InitializeComponent();
             
-            
+            /*
             myConnection = new SqlConnection("user id=admin3;" + // Username
                                   "password=admin;" + // Password
                                   "server=LAPTOP-6TEGHEN2;" + // Server name
@@ -30,7 +30,7 @@ namespace CMPT291_Project
                                   "database=Project_291; " + // Database
                                   "connection timeout=30"); // Timeout in seconds
             
-
+            */
             /*
             myConnection = new SqlConnection("user id=admin3;" + // Username
                                   "password=admin;" + // Password
